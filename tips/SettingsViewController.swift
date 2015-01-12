2cf1f1c326d6c4009f9bd60e0b2ffca8c9bca4af
Keita Ito <gi1okt@gmail.com>
2015-01-12 02:03:03 -0800

    second commit

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Keita on 1/12/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+import UIKit
+
+
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var dismissButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func dismissWindow(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        println("view will appear")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        println("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        println("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        println("view did disappear")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
 
+    var tipPercentages = [0.18, 0.2, 0.22]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(tipPercentages[0], forKey: "tipPercentage1")
+        defaults.setObject(tipPercentages[1], forKey: "tipPercentage2")
+        defaults.setObject(tipPercentages[2], forKey: "tipPercentage3")
+        defaults.synchronize()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+//        println("User editing bill")
+        
+//        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+//        println(tipPercentage)
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var doubleValue = defaults.doubleForKey("tipPercentage1")
+        println(doubleValue)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
