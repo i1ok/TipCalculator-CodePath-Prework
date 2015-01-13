65c73659f4f5ea068fbb188d4111e0475949a326
Keita Ito <gi1okt@gmail.com>
2015-01-13 00:57:25 -0800

    developed setting view and NSUserDefaults

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -13,20 +13,62 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var segmentField1: UITextField!
+    @IBOutlet weak var segmentField2: UITextField!
+    @IBOutlet weak var segmentField3: UITextField!
+    
+    var defaults = NSUserDefaults.standardUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        println("view did load")
         
+        // get values from NSUserDefaults
+        var doubleValue1 = defaults.doubleForKey("tipPercentage1")
+        var doubleValue2 = defaults.doubleForKey("tipPercentage2")
+        var doubleValue3 = defaults.doubleForKey("tipPercentage3")
+        println(doubleValue1)
+        println(doubleValue2)
+        println(doubleValue3)
+        
+        // update segment value field
+        segmentField1.text = String(format: "%.2f", doubleValue1)
+        segmentField2.text = String(format: "%.2f", doubleValue2)
+        segmentField3.text = String(format: "%.2f", doubleValue3)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onEditingChangedSegment1(sender: AnyObject) {
+        // convert number as text to double
+        var valueSegment1 = segmentField1.text._bridgeToObjectiveC().doubleValue
+        println(valueSegment1)
+        // put it into NSUserDefaults
+        defaults.setDouble(valueSegment1, forKey: "tipPercentage1")
+    }
     
+    @IBAction func onEditingChangedSegment2(sender: AnyObject) {
+        var valueSegment2 = segmentField2.text._bridgeToObjectiveC().doubleValue
+        println(valueSegment2)
+        defaults.setDouble(valueSegment2, forKey: "tipPercentage2")
+    }
+    
+    @IBAction func onEditingChangedSegment3(sender: AnyObject) {
+        var valueSegment3 = segmentField3.text._bridgeToObjectiveC().doubleValue
+        println(valueSegment3)
+        defaults.setDouble(valueSegment3, forKey: "tipPercentage3")
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    // View actions
+    
+    // close settingView when done button is touched
     @IBAction func dismissWindow(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
@@ -50,14 +92,4 @@ class SettingsViewController: UIViewController {
         super.viewDidDisappear(animated)
         println("view did disappear")
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -16,20 +16,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
 
+    // create tipPercentages array with initial tip values
     var tipPercentages = [0.18, 0.2, 0.22]
+    
+    // create NSUserDefaults
+    var defaults = NSUserDefaults.standardUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize labels
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
         
+        // Get values from NSUserDefaults for tipPercentages
+        var doubleValue1 = defaults.doubleForKey("tipPercentage1")
+        var doubleValue2 = defaults.doubleForKey("tipPercentage2")
+        var doubleValue3 = defaults.doubleForKey("tipPercentage3")
         
+        // put values to tipPercentages array
+        tipPercentages[0] = doubleValue1
+        tipPercentages[1] = doubleValue2
+        tipPercentages[2] = doubleValue3
         
-        var defaults = NSUserDefaults.standardUserDefaults()
+        // create key-value sets to NSUserDefaults
         defaults.setDouble(tipPercentages[0], forKey: "tipPercentage1")
-        defaults.setObject(tipPercentages[1], forKey: "tipPercentage2")
-        defaults.setObject(tipPercentages[2], forKey: "tipPercentage3")
+        defaults.setDouble(tipPercentages[1], forKey: "tipPercentage2")
+        defaults.setDouble(tipPercentages[2], forKey: "tipPercentage3")
         defaults.synchronize()
     }
 
@@ -41,27 +54,56 @@ class ViewController: UIViewController {
     @IBAction func onEditingChanged(sender: AnyObject) {
 //        println("User editing bill")
         
-//        var tipPercentages = [0.18, 0.2, 0.22]
+        // get one tip value from tipPercentages array to calculate
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
 //        println(tipPercentage)
-        
+
+        // calculate tip and total amount
         var billAmount = billField.text._bridgeToObjectiveC().doubleValue
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
+//        tipLabel.text = "$\(tip)"
+//        totalLabel.text = "$\(total)"
         
+        // update label
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
         
-        var defaults = NSUserDefaults.standardUserDefaults()
-        var doubleValue = defaults.doubleForKey("tipPercentage1")
-        println(doubleValue)
+//        var defaults = NSUserDefaults.standardUserDefaults()
+//        var doubleValue = defaults.doubleForKey("tipPercentage1")
+//        println(doubleValue)
     }
 
     @IBAction func onTap(sender: AnyObject) {
+        // dismiss keyboard
         view.endEditing(true)
+    }
+    
+    // when screen shows again rootView from settingView
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        println("rootView will appear")
+        
+        // get values from NSUserDefaults
+        var doubleValue1 = defaults.doubleForKey("tipPercentage1")
+        var doubleValue2 = defaults.doubleForKey("tipPercentage2")
+        var doubleValue3 = defaults.doubleForKey("tipPercentage3")
+        
+        // put values into tipPercentage array
+        tipPercentages[0] = doubleValue1
+        tipPercentages[1] = doubleValue2
+        tipPercentages[2] = doubleValue3
+        
+        // convert double values to int
+        var intValue1 = Int(doubleValue1 * 100)
+        var intValue2 = Int(doubleValue2 * 100)
+        var intValue3 = Int(doubleValue3 * 100)
+
+        // update tipControlSegment
+        tipControl.setTitle("\(intValue1)%" , forSegmentAtIndex: 0)
+        tipControl.setTitle("\(intValue2)%" , forSegmentAtIndex: 1)
+        tipControl.setTitle("\(intValue3)%" , forSegmentAtIndex: 2)
     }
 }
 
